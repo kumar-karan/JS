0f49549b8a85e4f89aafcd9af7385c67571928fb
kumar-karan <kk9910083222@gmail.com>
2023-12-18 23:33:14 +0530

    data types commit 2

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as new Version
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+console.log("Karan")
+
+let name = "Hitesh"
+let age = 21
+let isLoggedIn = false
+
+
+// Number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true or false
+// null -> stand alone value
+
+// undefined -> value is not defined 
+
+console.log(typeof undefined);
+console.log(typeof null);
