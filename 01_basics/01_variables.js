789ed50e404a1cc3eec03b8bd3468d4780e8f4c4
kumar-karan <kk9910083222@gmail.com>
2023-12-18 23:18:28 +0530

    data types commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 144553  // cannot be changed
+let accountEmail = "karan@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2  // NOT ALLOWED
+
+accountEmail = "k@k.com"
+accountPassword = "212121"
+accountCity = "Delhi"
+
+console.log(accountID);
+
+console.table([accountID, accountEmail, accountPassword, accountCity]);
